38ffa48859795c21500c62b9d64fa740b48aa951
Ashutosh <ashutosh@ost.com>
2018-03-29 12:43:29 +0530

    custom changes

--- app/assets/javascripts/web/common.js
+++ app/assets/javascripts/web/common.js
@@ -1,3 +1,4 @@
 //= require plugins/popper/popper
 //= require plugins/bootstrap/bootstrap
 //= require web/common/SimpleTokenNamespace
+//= require web/common/TopNavBar
--- app/assets/javascripts/web/ost-index.js
+++ app/assets/javascripts/web/ost-index.js
@@ -3,4 +3,3 @@
 //= require plugins/slick/slick
 //= require web/ost/home_carousel
 //= require web/ost/index
-//= require web/common/TopNavBar
