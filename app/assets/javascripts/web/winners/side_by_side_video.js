ffb9958676b7c42685abf1cbf9e5565822e535fc
thahir-pepo <thahir@pepo.com>
2018-06-06 16:41:06 +0530

    side by side

--- app/assets/javascripts/web/winners/side_by_side_video.js
+++ app/assets/javascripts/web/winners/side_by_side_video.js
@@ -40,7 +40,32 @@
         $(".participants-carousel").slick({
           slidesToShow: 3,
           slidesToScroll: 3,
-          speed: 500
+          speed: 500,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
         });
       }
 
