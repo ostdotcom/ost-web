3f0c7b356715a75ebd39d12bb21134ca05d11c7c
Ashutosh <ashutosh@ost.com>
2018-07-10 19:08:38 +0530

    CO-71

--- app/assets/javascripts/web/winners/sign_up_form.js
+++ app/assets/javascripts/web/winners/sign_up_form.js
@@ -9,95 +9,93 @@
     jSignUpForm: null,
 
     init: function (config) {
-      oThis.jSignUpForm = $('#sign-up-form');
-      oThis.jSignUpForm.setCustomValidity();
-      oThis.bindButtonActions();
-    },
-
-    bindButtonActions: function () {
-
-      $("#sign-up-form-submit").on("click", function (event) {
+      $("#subscribe-form-submit").on("click", function (event) {
         event.preventDefault();
-        oThis.onSignUpFormSubmit( event );
+        oThis.onSubscribe();
       });
-
-      $(".smooth-scroll").on('click', function (event) {
-        if (this.hash !== "") {
-          event.preventDefault();
-          var hash = this.hash;
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800);
-        }
-      });
-
     },
 
-    onSignUpFormSubmit: function () {
-
-      //Validate Everything Again
-      if ( !oThis.isSignUpFormValid() ) {
-        console.log("Validation failed!");
-        return;
-      }
-      $("#sign-up-form-submit")
-        .text('sending ...')
-        .prop( "disabled", true );
-      oThis.onSendMessage();
-    },
-
-    isSignUpFormValid: function () {
-      utilsNs.errorHandling.clearFormErrors();
-      oThis.jSignUpForm.find('input, textarea').trigger('change');
-      if(typeof oThis.jSignUpForm.find('.g-recaptcha')[0] != 'undefined' && typeof grecaptcha  != 'undefined'){
-        if(grecaptcha.getResponse() == ''){
-          oThis.jSignUpForm.find('.error[data-for="recaptcha"]').text('Please select the reCaptcha checkbox');
-        }
-      }
-      if(!$('#sign-up-form input[name=ost_product_communicate]').is(':checked')){
-        oThis.jSignUpForm.find('.error[data-for="ost_product_communicate"]').text('Please check the checkbox');
-      }
-      return oThis.jSignUpForm.find('.error:not(:empty)').length == 0;
-    },
-
-    onSendMessage: function () {
-      var $signupform = $('#sign-up-form'),
-          requestUrl = $signupform.prop('action'),
-          methodType = $signupform.prop('method')
+    onSubscribe: function () {
+      var oThis     = this,
+        jForm     = $('#subscribe-form'),
+        jSubmitBtn = $("#subscribe-form-submit"),
+        jsonpUrl  = jSubmitBtn.data('jsonp'),
+        jEmail    = $("#subscribe-form-email"),
+        emailVal   = jEmail.val().trim(),
+        emailPattern =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        isFormValid = true
       ;
-      $('#successModal').modal('hide');
+
+      oThis.resetError( jForm );
+
+
+      if( !emailVal ) {
+        oThis.showError('Email is mandatory', '.email-error');
+        isFormValid = false;
+      }else if ( !emailPattern.test( emailVal ) ) {
+        oThis.showError('Invalid email', '.email-error' );
+        isFormValid =  false;
+      }
+
+
+      if ( !$("input#subscribe_confirmation").is(":checked")){
+        oThis.showError('Please select the checkbox to continue', '.confirmation_error' );
+        isFormValid =  false;
+      }
+
+      if( !isFormValid ){
+        return false;
+      }
+
+      jSubmitBtn.text('Submitting...').prop('disabled', true);
+
+
       $.ajax({
-        url: requestUrl,
-        dataType: 'json',
-        method: methodType,
-        data: $signupform.serialize(),
-        success: function (response) {
-          if (response.success == true) {
-            $('#successModal').modal('show');
-            $signupform[0].reset();
+        url: jsonpUrl,
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {email: emailVal},
+        method: 'GET',
+        success: function (responseJson) {
+          if ((responseJson.error != undefined) && (responseJson.error != '')) {
+
+            var error_msg = [];
+            $.each(responseJson.error_message, function (errors_key, errors_value) {
+              $.each(errors_value, function (index, value) {
+                error_msg.push(value);
+              });
+            });
+
+            oThis.showError(error_msg.join('. '), '.general_error');
+
           } else {
-            utilsNs.errorHandling.displayFormErrors(response);
-            if(typeof grecaptcha  != 'undefined'){
-              grecaptcha.reset();
-            }
-          }
-        },
-        error: function (jqXHR, exception) {
-          utilsNs.errorHandling.xhrErrResponse(jqXHR, exception);
-        },
 
+            oThis.resetError( jForm );
+            jForm.hide();
+            $('#subscribe-success').show();
+          }
+
+        },
+        error: function (error) {
+          console.log("error in sign-up" , error);
+          oThis.showError('Something Went Wrong', '.general_error');
+        },
         complete: function (response) {
-          $("#sign-up-form-submit")
-            .text('submit')
-            .prop( "disabled", false );
-
-          if(typeof grecaptcha  != 'undefined'){
-            grecaptcha.reset();
-          }
+          jSubmitBtn.text('Sign Up').prop('disabled', false);
         }
 
       });
-    }
+    },
+
+    resetError: function ( jForm ) {
+      jForm.find('.error').html("");
+    },
+
+    showError: function (text, selector) {
+      $('#subscribe-form ' + selector).html(text);
+      $('#subscribe-form input').addClass('red');
+    },
+
 
   };
 
