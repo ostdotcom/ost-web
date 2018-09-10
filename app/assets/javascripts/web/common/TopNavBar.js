5869adc48481f015d7bcc270a7bd1c6ece4c5fc1
thahir-pepo <thahir@pepo.com>
2018-09-10 19:32:41 +0530

    nav bar UI

--- app/assets/javascripts/web/common/TopNavBar.js
+++ app/assets/javascripts/web/common/TopNavBar.js
@@ -1,11 +1,11 @@
-(function (window, $) {
-  $( function () {
-    $(".nav-products").mouseenter( function () {
-      $(this).find('.mega-dropdown-menu').addClass('open');
-    });
-    $(".nav-products").mouseleave( function () {
-      $(this).find('.mega-dropdown-menu').removeClass('open');
-    });
-  })
-
-})(window,jQuery)
+//(function (window, $) {
+//  $( function () {
+//    $(".nav-products").mouseenter( function () {
+//      $(this).find('.mega-dropdown-menu').addClass('open');
+//    });
+//    $(".nav-products").mouseleave( function () {
+//      $(this).find('.mega-dropdown-menu').removeClass('open');
+//    });
+//  })
+//
+//})(window,jQuery)
