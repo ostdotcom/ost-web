a351934c8bc69be5e66a1078bad3bbcb8f28f004
Shraddha <shraddha.patil@ost.com>
2018-10-24 12:07:06 +0530

    quarter scroll

--- app/assets/javascripts/web/roadmap/Index.js
+++ app/assets/javascripts/web/roadmap/Index.js
@@ -11,6 +11,7 @@
       oThis.fixedHeader();
       oThis.quarterScroll();
       oThis.categoriesToAnimate();
+      oThis.highlightQuarter();
     },
 
     bindButtonActions: function () {
@@ -81,6 +82,31 @@
         }else {
           
         }
+      });
+    },
+
+
+    highlightQuarter : function () {
+      var animationTimerOut ,
+          isVisible = false , id
+      ;
+      $(window).on("scroll resize" , function(){
+        var jEls =  $('.qElementsToAnimate') ;
+        clearTimeout( animationTimerOut );
+        animationTimerOut = setTimeout( function () {
+          console.log("Outsideforloop");
+          for( var cnt = 0 ;  cnt < jEls.length ;  cnt++ ){
+            console.log("In for loop");
+            isVisible =  jEls.eq( cnt ).visible();
+            if( isVisible ){
+              id =  jEls.eq( cnt ).attr('id');
+              $('.quarter-item a').removeClass('selected-item');
+              $('.time-line').removeClass('selected');
+              $("[data-id='" + id + "']").find('.smooth-scroll').addClass('selected-item').closest('.time-line').addClass('selected');
+              break ;
+            }
+          }
+        } ,  50 )
       });
     }
   };
