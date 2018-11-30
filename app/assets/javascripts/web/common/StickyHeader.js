1a1436a794c62d4bb03b77dad5288f5fd6061014
thahir-reddy <thahir@ost.com>
2018-11-30 13:33:39 +0530

    team page fixed navbar

--- app/assets/javascripts/web/common/StickyHeader.js
+++ app/assets/javascripts/web/common/StickyHeader.js
@@ -11,53 +11,61 @@
     },
 
     bindButtonActions: function () {
-      oThis.stickyNav();
+      var ostNavHeight = $('.ost-nav').height(),
+          navHeight    = $('.container-about-nav').height()
+        ;
 
+      oThis.stickyNav();
 
       var scrollLink = $('.scroll');
       // Smooth scrolling
       scrollLink.on('click', function(e) {
+        e.preventDefault();
         if (this.hash !== "") {
-          e.preventDefault();
           var hash = this.hash;
           $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top - ostNavHeight
           }, 800);
         }
       });
 
       $(window).scroll(function() {
-        var scrollbarLocation = $(this).scrollTop();
-
+        var windowScroll = $(this).scrollTop();
+        console.log("===============start===============");
         scrollLink.each(function() {
+          var sectionOffset = $(this.hash).offset().top - ostNavHeight ;
+          console.log("sectionOffset" , sectionOffset  );
+            console.log("windowScroll" , windowScroll  );
 
-          var sectionOffset = $(this.hash).offset().top - 20;
-
-          if ( sectionOffset <= scrollbarLocation ) {
-            $(this).parent().addClass('active');
-            $(this).parent().siblings().removeClass('active');
-          }
+            if ( sectionOffset <= windowScroll ) {
+                $('.container-about-nav').find('.nav-item').removeClass('active');
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+           }
         })
-
+          console.log("==============end===============");
       })
 
     },
 
+
+
     stickyNav: function(){
-      var navContainer =  $('.container-about-nav');
-      var navOffset = navContainer.offset().top;
-      navContainer.wrap('<div class="nav-placeholder"></div>');
-      $('.nav-placeholder').height(navContainer.outerHeight());
+      var ostNavHeight      = $('.ost-nav').height() ,
+          navContainer      = $('.container-about-nav') ,
+          navPhantom        = $('.container-about-nav-phantom'),
+          navOffset         = navContainer.offset().top ,
+          aboutNavHeight    = navContainer.height()
+      ;
       $(window).scroll(function(){
         var scrollPos = $(window).scrollTop();
-        if(scrollPos >= navOffset){
-          $('.container-about-nav').addClass("nav-sticky");
+        if(scrollPos > navOffset){
+            navPhantom.height( aboutNavHeight  );
+            navContainer.css({position: 'fixed', top: ostNavHeight - 2 , zIndex: 2,left: '0px', width: '100%'});
         } else{
-          $('.container-about-nav').removeClass("nav-sticky");
+            navPhantom.height( 0 );
+            navContainer.css({position: 'static'});
         };
-        if(navOffset < scrollPos){
-          $('.container-about-nav').find('.nav-item').removeClass('active');
-        }
       });
     }
 
