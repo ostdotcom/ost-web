7e7c7ba8e51461d5412a8f2d1119554b4ed31534
thahir-reddy <thahir@ost.com>
2019-03-20 19:03:17 +0530

    HP UI

--- app/assets/javascripts/web/ost-index.js
+++ app/assets/javascripts/web/ost-index.js
@@ -1,8 +1,8 @@
 //= require plugins/handlebars/handlebars.js
 //= require plugins/slick/slick
-//= require plugins/masonry/masonry.pkgd.js
+//= #require plugins/masonry/masonry.pkgd.js
 //= require plugins/jquery-visible/jquery.visible.js
 //= require web/ost/index
 //= require web/ost/home_carousel
 //= require web/ost/events_carousel
-//= require web/ost/team_grid
+//= #require web/ost/team_grid
