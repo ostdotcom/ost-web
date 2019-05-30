be253ac947deabe27d7b0df7452692c8f0a61f75
thahir-reddy <thahir@ost.com>
2019-05-30 17:10:33 +0530

    OST wallet page

--- app/controllers/web/ost_wallet_controller.rb
+++ app/controllers/web/ost_wallet_controller.rb
@@ -1,12 +1,14 @@
 class Web::OstWalletController < Web::BaseController
 
+  layout "ost-wallet"
+
   skip_before_action :basic_auth
 
   before_action :set_page_meta_info
 
   def launch
     @launch_data = JSON.parse(params[:ld])
-    if !@launch_data['ios_app_download_link'] || !@launch_data['android_app_download_link']
+    if !@launch_data['ios_app_download_link'] || !@launch_data['android_app_download_link'] || !@launch_data['token_name']
       render :file => 'public/404.html'
     else
       render 'web/ost_wallet/launch'
