98b9ffd0a1856eb1aa3711c30c4f819f5eaa7b27
Shraddha <shraddha.patil@ost.com>
2019-07-03 16:16:06 +0530

    minor change

--- app/assets/javascripts/web/common/Cookie.js
+++ app/assets/javascripts/web/common/Cookie.js
@@ -8,12 +8,13 @@
     reject_expiry : 183 * 86400000,
     jAcceptCookie: $('.accept-cookie-wrapper'),
     jBtnRejectCookie: $('.btn-reject-cookies'),
+    cookieKey: 'privacy_banner_dismiss',
 
     init: function (config) {
 
       oThis.config = config;
       var isRejectCookieSet = document.cookie.split(';').filter(function (item) {
-        return item.indexOf('privacy_banner_dismiss=true')>=0
+        return item.indexOf(oThis.cookieKey+'=true')>=0
       }).length;
 
       if(!isRejectCookieSet ){
@@ -25,7 +26,7 @@
     bindEvents : function(){
       oThis.jBtnRejectCookie.on('click',function () {
         var gdpr_reject_expiry = oThis.getGmtString(oThis.reject_expiry);
-        document.cookie = "privacy_banner_dismiss=true; expires="+gdpr_reject_expiry+"; domain=."+oThis.config.domain+";path=/";
+        document.cookie = oThis.cookieKey+"=true; expires="+gdpr_reject_expiry+"; domain=."+oThis.config.domain+";path=/";
         oThis.jAcceptCookie.hide();
       })
     },
