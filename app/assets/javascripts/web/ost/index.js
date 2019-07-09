60767c5578384b7e82bb01ab05cf70a780fab104
Shraddha <shraddha.patil@ost.com>
2019-07-09 15:53:02 +0530

    bug fix

--- app/assets/javascripts/web/ost/index.js
+++ app/assets/javascripts/web/ost/index.js
@@ -16,10 +16,15 @@
 
     bindButtonActions: function () {
 
-      $("#subscribe-form-email").on('input',function(){
+      $("#home-subscribe-form-email").on('input',function(){
           $(".checkbox-section").show();
           $('#subscribe-success-wallet-app').hide();
       });
+      $("#home-subscribe-form-submit").on("click", function (event) {
+        event.preventDefault();
+        oThis.onSubscribeHome();
+      });
+
       $("#subscribe-form-submit").on("click", function (event) {
         event.preventDefault();
         oThis.onSubscribe();
@@ -64,17 +69,17 @@
       }
     },
 
-    onSubscribe: function () {
+    onSubscribeHome(){
       var oThis     = this,
-          jForm     = $('#subscribe-form'),
-          jSubmitBtn = $("#subscribe-form-submit"),
-          jsonpUrl  = jSubmitBtn.data('jsonp'),
-          jEmail    = $("#subscribe-form-email"),
-          emailVal   = jEmail.val().trim(),
-          emailPattern =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          isFormValid = true,
-          platform_marketing = 0,
-          popcorn_wallet = 1
+        jForm     = $('#home-subscribe-form'),
+        jSubmitBtn = $("#home-subscribe-form-submit"),
+        jsonpUrl  = jSubmitBtn.data('jsonp'),
+        jEmail    = $("#home-subscribe-form-email"),
+        emailVal   = jEmail.val().trim(),
+        emailPattern =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        isFormValid = true,
+        platform_marketing = 0,
+        popcorn_wallet = 1
       ;
 
       oThis.resetError( jForm );
@@ -94,9 +99,9 @@
         platform_marketing = 1;
       }
 
-     if( !isFormValid ){
-         return false;
-     }
+      if( !isFormValid ){
+        return false;
+      }
 
       jSubmitBtn.prop('disabled', true);
       $(".checkbox-section").hide();
@@ -140,6 +145,77 @@
 
       });
     },
+    onSubscribe: function () {
+      var oThis     = this,
+        jForm     = $('#subscribe-form'),
+        jSubmitBtn = $("#subscribe-form-submit"),
+        jsonpUrl  = jSubmitBtn.data('jsonp'),
+        jEmail    = $("#subscribe-form-email"),
+        emailVal   = jEmail.val().trim(),
+        emailPattern =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        isFormValid = true
+      ;
+
+      oThis.resetError( jForm );
+
+
+      if( !emailVal ) {
+        oThis.showError('Email is mandatory', '.email-error');
+        isFormValid = false;
+      }else if ( !emailPattern.test( emailVal ) ) {
+        oThis.showError('Invalid email', '.email-error' );
+        isFormValid =  false;
+      }
+
+
+      if ( !$("input#subscribe_confirmation").is(":checked")){
+        oThis.showError('Please select the checkbox to continue', '.confirmation_error' );
+        isFormValid =  false;
+      }
+
+      if( !isFormValid ){
+        return false;
+      }
+
+      jSubmitBtn.text('Submitting...').prop('disabled', true);
+
+
+      $.ajax({
+        url: jsonpUrl,
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {email: emailVal},
+        method: 'GET',
+        success: function (responseJson) {
+          if ((responseJson.error != undefined) && (responseJson.error != '')) {
+
+            var error_msg = [];
+            $.each(responseJson.error_message, function (errors_key, errors_value) {
+              $.each(errors_value, function (index, value) {
+                error_msg.push(value);
+              });
+            });
+
+            oThis.showError(error_msg.join('. '), '.general_error');
+
+          } else {
+
+            oThis.resetError( jForm );
+            jForm.hide();
+            $('#subscribe-success').show();
+          }
+
+        },
+        error: function (error) {
+          console.log("error in sign-up" , error);
+          oThis.showError('Something Went Wrong', '.general_error');
+        },
+        complete: function (response) {
+          jSubmitBtn.text('Sign Up').prop('disabled', false);
+        }
+
+      });
+    },
 
     showError: function (text, selector) {
       $('#subscribe-form ' + selector).html(text);
@@ -153,70 +229,70 @@
       jForm[0].reset();
     },
 
-      dropDown: function(){
-          $('.dropdown-el').click(function(e) {
-              e.preventDefault();
-              e.stopPropagation();
-              $(this).toggleClass('expanded');
-              $('#'+$(e.target).attr('for')).prop('checked',true);
-              oThis.changeSection();
-          });
-          $(document).click(function() {
-              $('.dropdown-el').removeClass('expanded');
-          });
+    dropDown: function(){
+      $('.dropdown-el').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).toggleClass('expanded');
+        $('#'+$(e.target).attr('for')).prop('checked',true);
+        oThis.changeSection();
+      });
+      $(document).click(function() {
+        $('.dropdown-el').removeClass('expanded');
+      });
 
-          oThis.changeSection();
-      },
+      oThis.changeSection();
+    },
 
-      changeSection: function () {
-          var mobile_developer_tools = $('.mobile_developer_tools'),
-              mobile_btx_exchange = $('.mobile_btx_exchange'),
-              mobile_infrastructure = $('.mobile_infrastructure');
+    changeSection: function () {
+      var mobile_developer_tools = $('.mobile_developer_tools'),
+        mobile_btx_exchange = $('.mobile_btx_exchange'),
+        mobile_infrastructure = $('.mobile_infrastructure');
 
-          if($('#infrastructure').prop('checked')){
-              mobile_developer_tools.hide();
-              mobile_btx_exchange.hide();
-              mobile_infrastructure.show();
-          }
-          else if($('#developer-tools').prop('checked') ){
-              mobile_infrastructure.hide();
-              mobile_btx_exchange.hide();
-              mobile_developer_tools.show();
-          }
-          else if( $('#btx-exchange').prop('checked')   ){
-              mobile_infrastructure.hide();
-              mobile_developer_tools.hide();
-              mobile_btx_exchange.show();
-          }
-      },
-  
+      if($('#infrastructure').prop('checked')){
+        mobile_developer_tools.hide();
+        mobile_btx_exchange.hide();
+        mobile_infrastructure.show();
+      }
+      else if($('#developer-tools').prop('checked') ){
+        mobile_infrastructure.hide();
+        mobile_btx_exchange.hide();
+        mobile_developer_tools.show();
+      }
+      else if( $('#btx-exchange').prop('checked')   ){
+        mobile_infrastructure.hide();
+        mobile_developer_tools.hide();
+        mobile_btx_exchange.show();
+      }
+    },
+
     initClipPath : function () {
       if(oThis.isClipPathShapesSupported()){
         $('.hp-section-bg-wrap').addClass('polygon-init');
       }
     },
-  
+
     isClipPathShapesSupported : function () {
-  
+
       var base = 'clipPath',
         prefixes = [ 'webkit', 'moz', 'ms', 'o' ],
         properties = [ base ],
         testElement = document.createElement( 'testelement' ),
         attribute = 'polygon(50% 0%, 0% 100%, 100% 100%)';
-  
+
       // Push the prefixed properties into the array of properties.
       for ( var i = 0, l = prefixes.length; i < l; i++ ) {
         var prefixedProperty = prefixes[i] + base.charAt( 0 ).toUpperCase() + base.slice( 1 ); // remember to capitalize!
         properties.push( prefixedProperty );
       }
-  
+
       // Interate over the properties and see if they pass two tests.
       for ( var i = 0, l = properties.length; i < l; i++ ) {
         var property = properties[i];
-    
+
         // First, they need to even support clip-path (IE <= 11 does not)...
         if ( testElement.style[property] === '' ) {
-      
+
           // Second, we need to see what happens when we try to create a CSS shape...
           testElement.style[property] = attribute;
           if ( testElement.style[property] !== '' ) {
@@ -224,7 +300,7 @@
           }
         }
       }
-  
+
       return false;
     }
 
@@ -236,7 +312,7 @@
 
   $(window).on('scroll', function(e){
     var videoEl = $('#partner-video'),
-        mobileVideoEl = $('#partner-video-mobile');
+      mobileVideoEl = $('#partner-video-mobile');
     if(videoEl.length>0 && !videoEl.visible(true) ) {
       oThis.pauseVideo(videoEl);
     }
@@ -254,8 +330,8 @@
     oThis.pauseVideo(videoEl);
     oThis.pauseVideo(mobileVideoEl);
   });
-  
-  
+
+
 
 
 
