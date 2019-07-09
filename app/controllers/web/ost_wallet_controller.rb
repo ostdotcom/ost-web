d5e3a5ec5a47a71fc4c308edf90a29a85d3bb95a
pankaj <pankaj@ost.com>
2019-07-09 13:15:41 +0530

    Backend request

--- app/controllers/web/ost_wallet_controller.rb
+++ app/controllers/web/ost_wallet_controller.rb
@@ -7,6 +7,7 @@ class Web::OstWalletController < Web::BaseController
   before_action :set_page_meta_info, :except => [:send_popcorn_invite]
 
   def launch
+    (render :file => 'public/404.html' and return) if params[:ld].blank?
     @launch_data = JSON.parse(params[:ld])
     if !@launch_data['ios_app_download_link'] || !@launch_data['android_app_download_link'] || !@launch_data['token_name']
       render :file => 'public/404.html'
