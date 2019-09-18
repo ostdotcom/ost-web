29c11d50ee7f81c2a52dd53914f9bced63834085
Shraddha <shraddha.patil@ost.com>
2019-09-18 13:28:31 +0530

    use case section

--- app/assets/javascripts/web/ost/prototypes_carousal.js
+++ app/assets/javascripts/web/ost/prototypes_carousal.js
@@ -10,71 +10,102 @@
         snapElemetHeight  : 600,
         jumbotronHeight   : $('.side-by-side-1-wrapper').outerHeight(true),
         noOfPrototypes    : $('.snap-container ul li').length,
+        threshold         : 2,
+        startIndex        : 0,
+        jViewMoreBtn      :$('#view-more-prototypes'),
 
         init : function (config) {
           oThis.initPrototypeCarousal();
           oThis.viewMore();
           oThis.bindActions();
-
         },
 
         initPrototypeCarousal : function () {
-          $('#prototypes-carousal').slick({
-            dots: true,
-            arrows: false,
-            slidesToShow: 2,
-            centerMode:true,
-            initialSlide:0,
-            centerPadding:'200px',
+          if(navigator.userAgent.match(/iPad/i)){
+            $('#prototypes-carousal').slick({
+              dots: true,
+              arrows: false,
+              slidesToShow: 1,
+              centerMode:true,
+              initialSlide:0,
+              centerPadding:'300px',
 
-          });
+            });
 
-          $('.slick-current').prev('div').addClass('nofade');
-          // $('.slick-current').prev('div').animate({opacity :1});
+          }else{
+            $('#prototypes-carousal').slick({
+              dots: true,
+              arrows: false,
+              slidesToShow: 2,
+              centerMode:true,
+              initialSlide:0,
+              centerPadding:'200px',
+
+            });
+          }
+
+          if(!navigator.userAgent.match(/iPad/i)){
+            $('.slick-current').prev('div').addClass('nofade');
+          }else{
+            $('.slick-slide').css({'display': 'flex' , 'justify-content' : 'center'});
+          }
+
 
         },
         bindActions : function () {
           $('#prototypes-carousal').on('beforeChange',function (event,slick,currentSlide,nextSlide) {
             $('.slick-slide').removeClass('nofade');
               //add no fade class to nextslides's previous div
+            if(!navigator.userAgent.match(/iPad/i)){
               $("[data-slick-index='" + nextSlide +"']").prev('div').addClass('nofade');
-            })
-          var lastScroll = 0;
-          var scrollUp = false;
-
-          $(window).on('scroll ', function(){
-            oThis.scrollHeight = $(window).scrollTop();
-            if(oThis.jumbotronHeight < oThis.scrollHeight){
-              $('.snap-container').addClass('protoype-wrapper');
             }
 
+            })
 
+          oThis.jViewMoreBtn.on('click',function () {
+              oThis.viewMore();
           });
-
-
-
-
         },
         viewMore : function () {
-
+          var tempThreshold = oThis.threshold;
           if(oThis.noOfPrototypes > 3 ){
-            for( var i =0 ; i < 3 ; i++){
-              $('.snap-container ul li')[i].show();
+
+            $('.snap-container ul li').each(function (index) {
+              if(oThis.startIndex <= index && index < oThis.threshold){
+                $(this).show();
+                tempThreshold = index;
+              }
+            });
+
+            if(oThis.noOfPrototypes == tempThreshold + 1){
+              oThis.jViewMoreBtn.hide();
             }
+            oThis.startIndex = tempThreshold + 1;
+            oThis.threshold = tempThreshold + 3;
+
           }else{
-            for( var i =0 ; i < oThis.noOfPrototypes ; i++){
-              $('.snap-container ul li')[i].show();
-            }
+
+            oThis.jViewMoreBtn.hide();
+            $('.snap-container ul li').each(function (index) {
+              $(this).show()
+            });
           }
-
-
-
-
-
-
-
-
         }
       }
+
+      $(document).ready(function () {
+
+        // $('.iframe-dimensions').load(function() {
+        //   $(this).prev('img').hide();
+        //   $(this).show();
+        //
+        // });
+        //
+        // $('.iframe-dimensions-mobile').load(function() {
+        //   $(this).prev('img').hide();
+        //   $(this).show();
+        //
+        // });
+      })
 
 })(window,jQuery);
